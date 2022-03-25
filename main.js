6e19c912e043111a702d1e55be4e744441e8f554
Steven Woo <swoo9875@gmail.com>
2022-03-25 14:45:24 -0400

    Starting point

--- main.js
+++ main.js
@@ -71,3 +71,11 @@ function validateTrack(content){
 }
 
 client.login(process.env.TOKEN)
+
+client.on("message", msg => {
+	if (msg.content == "ping") {
+		msg.reply("pong");
+	}
+})
+
+client.login(process.env.TOKEN)
